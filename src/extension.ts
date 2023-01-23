452061186229a1e64c2172d3d2363df0b7f1d4aa
GuilhermeUrenha <guilherme.urenha@gmail.com>
2023-01-23 02:55:03 -0300

    Closure

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,15 @@ async function getResult(input: string) {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto('https://codebeautify.org/jsviewer');
-	console.log(page);
-return;
-	// Fill out the form with the input
-	await page.type('.ace_indentifier', input);
-	await page.click('#defaultActon');
+	await page.type('.ace_content', input);
 	
-	const result = await page.$eval('.ace_identifier', element => element.textContent);
+    var textContent:(string|null); //[]
+	textContent = (await page.$$eval('.ace_content', elements => { return elements.map(e => e.textContent); }))[1];
+	//divContent = await page.$$eval('.ace_identifier', elements => { return elements.map(e => e.textContent); }); //div#outputACEEditor >
+	console.log(textContent);
 
-//	// Wait for the result to be displayed
-//	await page.waitForSelector('#result');
-//	
-//	// Extract the result from the page
-//	const result = await page.$eval('#result', el => el.textContent);
-	
 	await browser.close();
-	return result;
+	return textContent;
 }
 
 export function activate(context: vscode.ExtensionContext) {
